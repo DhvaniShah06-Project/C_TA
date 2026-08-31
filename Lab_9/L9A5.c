09bc4158fb5c728d28b31f983030eb449a08b009
Dhvani Shah <dhvanihshah06@gmail.com>
2026-08-31 10:05:22 +0530

    While Loop

---
+++ Lab_9/L9A5.c
@@ -0,0 +1,16 @@
+// prints factors
+#include <stdio.h>
+void main()
+{
+    int n, i = 0;
+    printf("Enter n : ");
+    scanf("%d", &n);
+    while (i <= n)
+    {
+        i = i + 1;
+        if (n % i == 0)
+        {
+            printf("%d\n", i);
+        }
+    }
+}
