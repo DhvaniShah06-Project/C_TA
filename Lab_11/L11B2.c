d012014ecfc40eb1e0253d3ba834347f5e9aa6c7
Dhvani Shah <dhvanihshah06@gmail.com>
2026-09-07 19:43:44 +0530

    Lab-11

--- Lab_11/L11B2.c
+++ Lab_11/L11B2.c
@@ -1,45 +1,65 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "stdio.h"
 
-int main()
+void main()
 {
-    int n, temp, rem;
-
-    printf("Enter an integer: ");
+    int n, num, c0 = 0, c1 = 0, c2 = 0, c3 = 0, c4 = 0, c5 = 0, c6 = 0, c7 = 0, c8 = 0, c9 = 0;
+    printf("Enter number : ");
     scanf("%d", &n);
-
-    printf("\nFrequency of digits in %d:\n", n);
-
-    for (int digit = 0; digit <= 9; digit++)
+    for (int i = n; i > 0; i /= 10)
     {
-        int count = 0;
-        temp = abs(n); // Reset to original input and handle negative numbers
-
-        // Special case: handle input 0 for the digit 0
-        if (temp == 0 && digit == 0)
+        num = i % 10;
+        switch (num)
         {
-            count = 1;
-        }
-        else
-        {
-            // Loop through the integer to count current digit matches
-            while (temp != 0)
-            {
-                rem = temp % 10;
-                if (rem == digit)
-                {
-                    count++;
-                }
-                temp = temp / 10;
-            }
-        }
-
-        // Print final result only if the digit exists in the number
-        if (count > 0)
-        {
-            printf("Digit %d occurs %d time\n", digit, count);
+        case 0:
+            c0++;
+            break;
+        case 1:
+            c1++;
+            break;
+        case 2:
+            c2++;
+            break;
+        case 3:
+            c3++;
+            break;
+        case 4:
+            c4++;
+            break;
+        case 5:
+            c5++;
+            break;
+        case 6:
+            c6++;
+            break;
+        case 7:
+            c7++;
+            break;
+        case 8:
+            c8++;
+            break;
+        case 9:
+            c9++;
+            break;
         }
     }
-
-    return 0;
+    if (c0 > 0)
+        printf("Frequency of 0 : %d\n", c0);
+    if (c1 > 0)
+        printf("Frequency of 1 : %d\n", c1);
+    if (c2 > 0)
+        printf("Frequency of 2 : %d\n", c2);
+    if (c3 > 0)
+        printf("Frequency of 3 : %d\n", c3);
+    if (c4 > 0)
+        printf("Frequency of 4 : %d\n", c4);
+    if (c5 > 0)
+        printf("Frequency of 5 : %d\n", c5);
+    if (c6 > 0)
+        printf("Frequency of 6 : %d\n", c6);
+    if (c7 > 0)
+        printf("Frequency of 7 : %d\n", c7);
+    if (c8 > 0)
+        printf("Frequency of 8 : %d\n", c8);
+    if (c9 > 0)
+        printf("Frequency of 9 : %d\n", c9);
 }
